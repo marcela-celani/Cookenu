60f55fbada809de0190a25d3cc25c76c0f38f10c
marcela-celani <marcelacelani.ti@gmail.com>
2023-08-08 11:17:17 -0300

    Adjusting files

--- my-cookenu/src/routes/router.js
+++ my-cookenu/src/routes/router.js
@@ -14,8 +14,8 @@ export const Router = () => {
                 <Route path='/login' element={<LoginPage/>} />
                 <Route path='/signup' element={<SignUpPage/>} />
                 <Route path='/feed' element={<FeedPage/>} />
-                <Route path='/add-recipe' element={<RecipeDetailPage/>} />
-                <Route path='/recipe/:id' element={<AddRecipePage/>} />
+                <Route path='/add-recipe' element={<AddRecipePage/>} />
+                <Route path='/recipe/:id' element={<RecipeDetailPage/>} />
             </Routes>
         </BrowserRouter>
     )
